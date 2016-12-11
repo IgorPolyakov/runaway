d80b2e7b526b96a6abcaef240e63ef1840487cb4
IgorPolyakov <fox.user.3@gmail.com>
2016-12-11 21:45:12 +0700

    add profile avatar && fix css

--- app/helpers/track_helper.rb
+++ app/helpers/track_helper.rb
@@ -1,2 +1,7 @@
 module TrackHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    # "http://gravatar.com/avatar/.png"
+    "https://robohash.org/#{gravatar_id}.png"
+  end
 end
